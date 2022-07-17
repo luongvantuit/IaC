f9dfa378ff2c0cd7b03d9f8261c97586207d2ce1
Lương Văn Tú <luongvantuit@gmail.com>
2022-07-17 23:59:53 +0700

    provisioner inline setup ansible for ec2 remote

--- ec2_remote.tf
+++ ec2_remote.tf
@@ -16,7 +16,17 @@ resource "aws_instance" "instance_remote" {
 
   provisioner "remote-exec" {
     inline = [
-      "sudo apt update"
+      "echo \"--- Apply Updates on Control Node ---\"",
+      "sudo apt update",
+      "sudo apt updgrade -y",
+      "echo \"--- Install dependencies and configure Ansible Repository ---\"",
+      "sudo apt install -y software-properties-common",
+      "sudo add-apt-repository --yes --update ppa:ansible/ansible",
+      "sudo apt update",
+      "echo \"Install latest version of Ansible\"",
+      "sudo apt install ansible -y",
+      "echo \"--- Check verison Ansible ---\"",
+      "ansible --version"
     ]
     connection {
       host        = self.public_ip
